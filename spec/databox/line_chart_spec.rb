e0c9d9921c5401dd6a11cf7dbbf80f4aaaf87bd6
Oto Brglez <otobrglez@gmail.com>
2014-02-03 17:41:42 +0100

    Removing little code.

--- spec/databox/line_chart_spec.rb
+++ spec/databox/line_chart_spec.rb
@@ -2,14 +2,6 @@ require "spec_helper"
 
 describe Databox::LineChart do
 
-
-  before do
-    Databox.configure do |c|
-      c.token   = "1cgeco9oc5us88ss"
-      c.key = "5u9s6ynhv0o44g4co8w00gw84cwgs0gw"
-    end
-  end
-
   let(:chart){ Databox::LineChart.new "my_line" }
 
   context "simple" do
