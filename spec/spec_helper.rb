d44611dddeb527ef14abcca5371e948b87f7028c
Oto Brglez <otobrglez@gmail.com>
2015-06-04 16:32:27 +0200

    Travis CI update.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ RSpec.configure do |config|
   config.fail_fast = ENV['RSPEC_FAIL_FAST'] == "1"
   config.mock_framework = :rspec
   config.run_all_when_everything_filtered = true
-  config.treat_symbols_as_metadata_keys_with_true_values = true
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
